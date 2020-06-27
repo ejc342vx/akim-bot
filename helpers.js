8e3cfb07a42eabdda279963baa529fc349c1fd01
Daria Martynova <d.martynova@devinotele.com>
2020-06-27 21:22:04 +0300

    initial commit

---
+++ helpers.js
@@ -0,0 +1,47 @@
+getNoun = (number, one, two, five) => {
+  let n = Math.abs(number);
+  n %= 100;
+  if (n >= 5 && n <= 20) {
+      return five;
+  }
+  n %= 10;
+  if (n === 1) {
+      return one;
+  }
+  if (n >= 2 && n <= 4) {
+      return two;
+  }
+  return five;
+}
+
+time = (seconds) => {
+  if (seconds < 60) {
+      return `${getNoun(seconds, 'прошла', 'прошло', 'прошло')} ${seconds} ${getNoun(seconds, 'секунда', 'секунды', 'секунд')} с последнего обсуждения Акима!`
+  } else if (seconds < 3600) {
+      const m = Math.floor(seconds / 60)
+      return `${getNoun(m, 'прошла', 'прошло', 'прошло')} ${m} ${getNoun(m, 'минута', 'минуты', 'минут')} с последнего обсуждения Акима!`
+  } else if (seconds >= 3600) {
+      let h = seconds / 3600 ^ 0
+      let m = (seconds - h * 3600) / 60 ^ 0
+      return `${getNoun(h, 'прошел', 'прошло', 'прошло')} ${h} ${getNoun(h, 'час', 'часа', 'часов')} ${m} ${getNoun(m, 'минута', 'минуты', 'минут')} с последнего обсуждения Акима!`
+  }
+}
+
+maxTime = (seconds) => {
+  if (seconds < 60) {
+      return `${seconds} ${getNoun(seconds, 'секунда', 'секунды', 'секунд')} - рекорд чата без обсуждения Акима`
+  } else if (seconds < 3600) {
+      const m = Math.floor(seconds / 60)
+      return `${m} ${getNoun(m, 'минута', 'минуты', 'минут')} - рекорд чата без обсуждения Акима`
+  } else if (seconds >= 3600) {
+      let h = seconds / 3600 ^ 0
+      let m = (seconds - h * 3600) / 60 ^ 0
+      return `${h} ${getNoun(h, 'час', 'часа', 'часов')} ${m} ${getNoun(m, 'минута', 'минуты', 'минут')} - рекорд чата без обсуждения Акима`
+  }
+}
+
+module.exports = {
+  time: time,
+  getNoun: getNoun,
+  maxTime: maxTime
+}
---
+++ index.js
@@ -0,0 +1,100 @@
+require('dotenv').config();
+
+const helper = require('./helpers')
+
+const {
+    Telegraf
+} = require('telegraf')
+const Extra = require('telegraf/extra')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mongoDB = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('error', err => {
+    console.error(
+        `Error occurred during an attempt to establish connection with the database`,
+        err
+    );
+    process.exit(1);
+});
+
+mongoose.connection.on('open', () => {
+
+    const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+
+    const AkimSchema = new Schema({
+        chatId: {
+            type: Number,
+            unique: true
+        },
+        maxTime: {
+            type: Number,
+        },
+        lastAkimMessage: {
+            type: Number,
+        },
+        stevenCounter: {
+            type: Number,
+        },
+        messageCounter: {
+            type: Number
+        }
+    });
+
+    const Akim = mongoose.model('Akim', AkimSchema, 'akim');
+    const words = ['Омерзительно.', 'Отвратительно.', 'Маразм.', 'Бред какой-то.', 'Наркоманы какие-то.']
+    const regex = /(^|\s)аким|асланов|akim|aslanov|aким?\D(?=\s|$)/gi
+
+    bot.command('akimstats', async (ctx) => {
+        const doc = await Akim.findOne({
+            chatId: ctx.message.chat.id
+        });
+        if (doc && doc.maxTime) {
+            ctx.reply(helper.maxTime(doc.maxTime))
+        }
+    })
+
+    bot.on(['text', 'photo', 'sticker', 'audio', 'video', 'document', 'forward'], async (ctx) => {
+        const doc = await Akim.findOne({
+            chatId: ctx.message.chat.id
+        });
+        if (doc) {
+            doc.messageCounter += 1
+            if (doc.messageCounter > 120) {
+                doc.messageCounter = 0
+                ctx.reply(words[Math.floor(Math.random() * words.length)], Extra.inReplyTo(ctx.update.message.message_id))
+            }
+
+            if (ctx.update.message.from.id == 366160795) {
+                doc.stevenCounter += 1
+                if (doc.stevenCounter > 13) {
+                    doc.stevenCounter = 0
+                    ctx.reply('Держи в курсе.', Extra.inReplyTo(ctx.update.message.message_id))
+                }
+            }
+
+            if (ctx.message.text.match(regex) || ctx.message.text.match(regex)) {
+                ctx.reply(helper.time(ctx.message.date - doc.lastAkimMessage))
+                const lastInterval = ctx.message.date - doc.lastAkimMessage
+                doc.maxTime = doc.maxTime > lastInterval ? doc.maxTime : lastInterval
+                doc.lastAkimMessage = ctx.message.date
+            }
+            doc.save()
+        } else {
+            const Akim1 = new Akim({
+                chatId: ctx.message.chat.id,
+                lastAkimMessage: ctx.message.date,
+                maxTime: 0,
+                messageCounter: 1,
+                stevenCounter: 0
+            })
+            Akim1.save()
+        }
+    })
+    bot.launch()
+})
